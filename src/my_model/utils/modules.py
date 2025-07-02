d05b1cd71a7f3d3cd8d01ad882e1ff4bad99f63d
Jeremy Couthures <jeremy.couthures@lapp.in2p3.fr>
2025-07-02 14:32:10 +0200

    Set a minimum learning rate

--- src/my_model/utils/modules.py
+++ src/my_model/utils/modules.py
@@ -197,9 +197,10 @@ class TransformerEncoder(nn.Module):
 
 class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
 
-    def __init__(self, optimizer, warmup, max_iters):
+    def __init__(self, optimizer, warmup, max_iters, min_lr=0.0):
         self.warmup = warmup
         self.max_iters = max_iters
+        self.min_lr = min_lr
         # if hasattr(trainer.train_dataloader, '__len__'):
         #     self.max_num_iters = trainer.max_epochs * len(trainer.train_dataloader)
         # else:
@@ -208,7 +209,7 @@ class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
 
     def get_lr(self):
         lr_factor = self.get_lr_factor(epoch=self.last_epoch)
-        return [base_lr * lr_factor for base_lr in self.base_lrs]
+        return [max(base_lr * lr_factor, self.min_lr) for base_lr in self.base_lrs]
 
     def get_lr_factor(self, epoch):
         lr_factor = 0.5 * (1 + np.cos(np.pi * epoch / self.max_iters))
